3bca97a69a50a1ea3f161654cda2be77ec404a58
Felix12232 <felixpa2001@gmail.com>
2025-11-20 13:30:25 +0100

    Le puse una fuente de letra

--- src/main/java/com/fburgos/logindigitalizacion/Vbox.java
+++ src/main/java/com/fburgos/logindigitalizacion/Vbox.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Vbox extends Application {
@@ -22,24 +23,27 @@ public class Vbox extends Application {
     public void start(Stage stage) throws Exception {
         VBox root = new VBox();
 
-        Label title = new Label("-------------------- LOGIN DAM ------------------ ");
+        Label title = new Label(" LOGIN DAM  ");
+        title.setFont(Font.font(35));
         root.getChildren().add(title);
         VBox.setMargin(title, new Insets(5,5,5,5));
 
         Label user = new Label(" User ");
+        user.setFont(Font.font("Roboto", 20));
          root.getChildren().add(user);
 
         TextField textField = new TextField();
         root.getChildren().add(textField);
-        VBox.setMargin(textField, new Insets(10,10,10,10));
+        VBox.setMargin(textField, new Insets(10,50,10,50));
         //Insercion de password
         Label password = new Label(" Password ");
+        password.setFont(Font.font(20));
         root.getChildren().add(password);
         VBox.setMargin(password, new Insets(10,10,10,10));
 
         PasswordField passwordField = new PasswordField();
         root.getChildren().add(passwordField);
-        VBox.setMargin(passwordField, new Insets(10,10,10,10));
+        VBox.setMargin(passwordField, new Insets(10,50,10,50));
 
         //INSERCIÓN BOTÓN
         Button btn = new Button("Enviar");
